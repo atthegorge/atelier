94dd56531c948c0484921ee8718470b661aa5d04
Masato Ushimaru <MasatoUshimaru@ARENDT.local>
2017-10-13 13:33:47 +0900

    投稿ページ、投稿完了ページの実装

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,5 +1,19 @@
 class JobsController < ApplicationController
   def index
-    @jobs = Job.all
+    @jobs = Job.order("created_at DESC")
   end
+
+  def new
+  end
+
+  def create
+    Job.create(job_params)
+    binding.pry
+  end
+
+  private
+  def job_params
+    params.permit(:title, :introduction, :description, :skill)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   root 'jobs#index'
-  get 'jobs' => 'jobs#index'
+  get   'jobs'      =>   'jobs#index'
+  get   'jobs/new'  =>   'jobs#new'
+  post  'jobs'      =>   'jobs#create'
 end
