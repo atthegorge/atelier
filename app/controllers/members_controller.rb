d0e59635bacce737de865997b272a631a61e3abf
Masato Ushimaru <MasatoUshimaru@ARENDT.local>
2017-10-15 22:43:57 +0900

    my page  and the view

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -1,5 +1,8 @@
 class MembersController < ApplicationController
 
   def show
+    # このユーザーにひもづく募集のみ表示する必要がある！
+    @jobs = Job.order("created_at DESC").page(params[:page]).per(5)
+    @members = Member.find(params[:id])
   end
 end
