fe192b73b81a2ff7bde8ad6eedea312e15f4b0ec
Masato Ushimaru <MasatoUshimaru@ARENDT.local>
2017-10-15 15:02:58 +0900

    members controller generated

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
   get   'jobs'      =>   'jobs#index'
   get   'jobs/new'  =>   'jobs#new'
   post  'jobs'      =>   'jobs#create'
-  get   'jobs/:id' =>   'jobs#show'
+  get   'jobs/:id'  =>   'jobs#show'
+  get   'members/:id' => 'members#show'
 end
